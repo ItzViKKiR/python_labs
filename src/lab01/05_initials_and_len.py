62efc06c95a00e707c48d83af36ad599cc17c271
ItzViKKiR <nguyenyenthanhthao18@gmail.com>
2025-09-11 20:34:43 +0300

    Лабораторная работа 1

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -2,6 +2,6 @@ print('Давайте узнаем ваши инициалы')
 fullname=str(input('ФИО:'))
 name=fullname.split(' ')
 newname=[new for new in name if new]
-initials=[newname[num][0] for num in range(len(newname))]
+initials=[newname[num][0].upper() for num in range(len(newname))]
 print('Инициалы: ','.'.join(initials))
 print('Длина (символов): ',len(' '.join(newname)))
